509b3d0d988ba50946a8e567bb100971fd131250
Adi Bar Ilan <adib@securitydam.com>
2022-02-08 07:42:49 +0200

    Add day 5

--- day 05/solution.py
+++ day 05/solution.py
@@ -1,17 +1,47 @@
+from hashlib import md5
+
+
+def get_password(inital):
+    final_str = ''
+    i = 0
+    while len(final_str) < 8:
+        i += 1
+        hash = md5((inital + str(i)).encode('utf-8')).hexdigest()
+        if hash[:5] == '00000':
+            print(hash)
+            yield hash[5]
+            final_str += hash[5]
+
+
 def q1():
-    with open('day 05\input.txt', 'r') as f:
-        input = f.read().splitlines()
+    input = 'ugkcyxxp'
+    return ''.join(list(get_password(input)))
+
+
+def get_ordered_password(inital):
+    final_str = [''] * 8
+    i = 0
+    while '' in final_str:
+        i += 1
+        hash = md5((inital + str(i)).encode('utf-8')).hexdigest()
+        if hash[:5] == '00000' and hash[5] in ('0', '1', '2', '3', '4', '5', '6', '7'):
+            print(hash)
+            loc, val = int(hash[5]), hash[6]
+            if final_str[loc] == '':
+                final_str[loc] = val
+
+    return ''.join(final_str)
 
 
 def q2():
-    with open('day 05\input.txt', 'r') as f:
-        input = f.read().splitlines()
+    input = 'ugkcyxxp'
+    return get_ordered_password(input)
 
 
 if __name__ == '__main__':
     from time import perf_counter as pc
     st = pc()
-    print(f'Part 1: {q1()}')
+    # print(f'Part 1: {q1()}')
     pt1 = pc()
     print(f'Part 2: {q2()}')
     pt2 = pc()
