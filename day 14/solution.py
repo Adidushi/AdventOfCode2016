fca150d1b14994e50ddc6b7debe5eb3737461828
Adi Bar Ilan <adiba@radware.com>
2022-06-15 08:29:47 +0300

    Add day 14

--- day 14/solution.py
+++ day 14/solution.py
@@ -1,11 +1,53 @@
+from functools import cache
+import hashlib
+import re
+
+salt = 'cuanljph'
+pattern = re.compile(r'(.)\1{2,}')
+
+
+@cache
+def hashed(in_str, iterations=1):
+    h = in_str
+    for _ in range(iterations):
+        h = hashlib.md5(h.encode('utf-8')).hexdigest()
+    return h
+
+
+@cache
+def is_hash_valid(index, iterations=1):
+    in_str = salt + str(index)
+    hash = hashed(in_str, iterations=iterations)
+    match = pattern.search(hash)
+    if not match:
+        return
+    to_match = match.group()[0] * 5
+    for index in range(index+1, index+1000):
+        if to_match in hashed(salt + str(index), iterations=iterations):
+            return True
+    return False
+
+
 def q1():
-    with open('day 14\input.txt', 'r') as f:
-        input = f.read().splitlines()
+    index = 0
+    found = list()
+    while True:
+        if is_hash_valid(index):
+            found.append(index)
+            if len(found) == 64:
+                return index
+        index += 1
 
 
 def q2():
-    with open('day 14\input.txt', 'r') as f:
-        input = f.read().splitlines()
+    index = 0
+    found = list()
+    while True:
+        if is_hash_valid(index, iterations=2017):
+            found.append(index)
+            if len(found) == 64:
+                return index
+        index += 1
 
 
 if __name__ == '__main__':
