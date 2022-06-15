20c2fb62b46534033ea91292f4c2921db8449ce1
Adi Bar Ilan <adiba@radware.com>
2022-06-15 08:38:24 +0300

    Add day 15

--- day 15/solution.py
+++ day 15/solution.py
@@ -1,11 +1,49 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Disc:
+    positions: int
+    start_position: int
+    index: int
+
+    def calc_position(self, time):
+        return (self.start_position + time + self.index + 1) % self.positions
+
+
 def q1():
     with open('day 15\input.txt', 'r') as f:
         input = f.read().splitlines()
+    discs = list()
+    for index, line in enumerate(input):
+        positions, start_position = int(line.split(
+            ' ')[3]), int(line[:-1].split(' ')[-1])
+        discs.append(Disc(positions, start_position, index))
+
+    time = 0
+    while True:
+        if all(disc.calc_position(time) == 0 for disc in discs):
+            return time
+        time += 1
 
 
 def q2():
     with open('day 15\input.txt', 'r') as f:
         input = f.read().splitlines()
+
+    discs = list()
+    for index, line in enumerate(input):
+        positions, start_position = int(line.split(
+            ' ')[3]), int(line[:-1].split(' ')[-1])
+        discs.append(Disc(positions, start_position, index))
+
+    discs.append(Disc(11, 0, len(discs)))
+
+    time = 0
+    while True:
+        if all(disc.calc_position(time) == 0 for disc in discs):
+            return time
+        time += 1
 
 
 if __name__ == '__main__':
